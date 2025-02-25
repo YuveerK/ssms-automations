e88beae15e750a6b582887434a86a67ff47e91f8
YuveerK <yuveerkal01@gmail.com>
2025-02-25 18:32:28 +0200

    Create SimpleDB with two entries

--- DatabaseAdministration/SQLStatements/test.sql
+++ DatabaseAdministration/SQLStatements/test.sql
@@ -1,1 +1,23 @@
 --This is a test script
+
+CREATE DATABASE SimpleDB;
+GO
+
+USE SimpleDB;
+GO
+
+CREATE TABLE Employees (
+	EmployeeID INT IDENTITY(1,1), PRIMARY KEY,
+	FirstName NVARCHAR(50),
+	LastName NVARCHAR(50)
+);
+
+GO
+
+INSERT INTO Employees (FirstName, LastName)
+VALUES ('John','Doe'),
+		('Jane','Doe');
+
+GO
+
+
